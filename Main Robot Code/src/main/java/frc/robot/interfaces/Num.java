e424c9089190ace859066152a0fb073b552c3174
Untitled13 <38769355+Untitled13@users.noreply.github.com>
2020-02-13 16:04:58 -0600

    Update Num.java

--- Main Robot Code/src/main/java/frc/robot/interfaces/Num.java
+++ Main Robot Code/src/main/java/frc/robot/interfaces/Num.java
@@ -1,20 +1,20 @@
 
 
-// public interface Num<A implements Num<A>> {
-//     A add(A that);
-//     A negate(); // opposite, negative, multiply by -1 addative inverse
-//     default A sub(A that) {
-//         return this.add(that.negate());
-//     }
-//     A invert(); // multiplicative inverse, (1/this)
-//     A mul(A that);
-//     default A div(A that) {
-//         return this.mul(that.invert());
-//     }
-//     A sgn();
-//     default A sqr() {
-//         return this.mul(this);
-//     }
-//     A sqrt();
+public interface Num<A implements Num<A>> {
+  A add(A that);
+       A negate(); // opposite, negative, multiply by -1 addative inverse
+       default A sub(A that) {
+           return this.add(that.negate());
+       }
+       A invert(); // multiplicative inverse, (1/this)
+       A mul(A that);
+       default A div(A that) {
+           return this.mul(that.invert());
+       }
+       A sgn();
+       default A sqr() {
+           return this.mul(this);
+       }
+     A sqrt();
 
-// }
+ }
