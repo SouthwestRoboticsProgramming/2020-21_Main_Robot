35108407bd6108805ba64d02888361eb32d59fef
Untitled13 <38769355+Untitled13@users.noreply.github.com>
2020-02-13 17:26:57 -0600

    Update Xbox.java

--- Main Robot Code/src/main/java/frc/robot/controllers/Xbox.java
+++ Main Robot Code/src/main/java/frc/robot/controllers/Xbox.java
@@ -20,84 +20,32 @@ import frc.lib.Looper.Loop;
 import frc.lib.Looper.Looper;
 import frc.robot.interfaces.IController;
 import frc.robot.subsystems.DriveTrainSubsystem;;
-public class Xbox implements IController {
+public class Xbox implements Controller {
     final int PORT;
     private XboxController xbox;
-    private double reverseVal = 1;
-    private Looper rumbleLooper;
 
     public Xbox(int port) {
         PORT = port;
         xbox = new XboxController(PORT);
+		init();
     }
+	final Control<R<Doub>> drive = new Control<R<Doub>>() {
+		public boolean get() {
+			return true;
+		}
+		public R<Doub> out() {
+			return new R<Doub>(xbox.getX(Hand.kLeft), xbox.getY(Hand.kLeft) );
+		}
+	}
+	
+	
 
-    public void defaultDrive(DriveTrainSubsystem train) {
-        
-    }
-
-    public double getAnalog(Hand hand, AxisType axis) {
-        double output = 0;
-        switch(axis) {
-            case kX : output = xbox.getX(hand); break;
-            case kY : output = xbox.getY(hand); break;
-        }
-        return output * reverseVal;
-    }
-    public JoystickButton getButton(Button button) {
-        switch(button) {
-            case intake : return new JoystickButton(xbox, XboxMap.A.toInt());
-            case hold : return new JoystickButton(xbox, XboxMap.B.toInt());
-            case unloadIntake : return new JoystickButton(xbox, XboxMap.X.toInt());
-            case unloadOutput : return new JoystickButton(xbox, XboxMap.Y.toInt());
-            case wheelSpin : return new JoystickButton(xbox, XboxMap.Select.toInt());
-            case wheelPosition : return new JoystickButton(xbox, XboxMap.Windows.toInt());
-            default         : return new JoystickButton(xbox, XboxMap.X.toInt());
-        }
-    }
     
-    public void rumble(RumbleType type, long ms) {
-        TimeOutTimer timer = new TimeOutTimer(ms);
-        Loop loop = new Loop(){
-            @Override public void onStart() {
-                xbox.setRumble(type, 1);
-                timer.start();
-            }
-            @Override public void onLoop() {
-              if (timer.getTimedOut()) {rumbleLooper.stop();}
-            }
-            @Override public void onStop() {
-                timer.stop();
-            }
-          };
-          rumbleLooper = new Looper(loop, 100);
-          rumbleLooper.start();
-        
-    }
-    public double getTrigger(Hand hand) {
-        return xbox.getTriggerAxis(hand);
-    }
-	private enum XboxMap {
-		A(1),
-		B(2),
-		X(3),
-		Y(4),
-		LeftTrigger(5),
-		RightTrigger(6),
-		Windows(7),
-		Select(8),
-		LeftJoystick(9),
-		RightJoystick(10);
-		private int num;
-		XboxMap(int buttonNumber)
-		{
-			num = buttonNumber;
-		}
-		public int toInt()
-		{
-			return num;
-		}
-    }
-    public void reverse() {
-        reverseVal = -Math.signum(reverseVal);
-    }
-}
+    public void rumble(double intensity) {
+	// something;
+	}
+	public void lights(int id, boolean state) {
+	// put stuff here
+	}
+	
+ }
